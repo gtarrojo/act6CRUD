ab467b9bd039093c08d1b4ee5b133f148255c0d2
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-03-21 17:25:44 +0100

    start ver detalle button logic

--- src/app/shared/buttons/buttons.component.ts
+++ src/app/shared/buttons/buttons.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IUser } from '../../interfaces/iuser.interface';
 
 @Component({
   selector: 'app-buttons',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './buttons.component.html',
-  styleUrl: './buttons.component.css'
+  styleUrl: './buttons.component.css',
 })
 export class ButtonsComponent {
-
+  @Input() user!: IUser;
 }
