3e19ef42b7decace11311923861c3c92583d3e05
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-03-21 16:53:59 +0100

    add buttons component

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { IUser } from '../../interfaces/iuser.interface';
+import { ButtonsComponent } from '../../shared/buttons/buttons.component';
 
 @Component({
   selector: 'app-user-card',
-  imports: [],
+  imports: [ButtonsComponent],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css',
 })
