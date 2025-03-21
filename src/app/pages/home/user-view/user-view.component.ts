737a2ce349ae707703ac6cdbf199e3a2d33cefc8
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-03-21 17:59:19 +0100

    start userView component logic

--- src/app/pages/home/user-view/user-view.component.ts
+++ src/app/pages/home/user-view/user-view.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { IUser } from '../../../interfaces/iuser.interface';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   selector: 'app-user-view',
   imports: [],
   templateUrl: './user-view.component.html',
-  styleUrl: './user-view.component.css'
+  styleUrl: './user-view.component.css',
 })
 export class UserViewComponent {
-
+  @Input() idUser: string = '';
+  user: IUser | any;
+  userService = inject(UsersService);
 }
