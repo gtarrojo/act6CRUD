8daada3036811ec832a7da1701b85af6876a5a9c
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-03-22 19:14:13 +0100

    add form logic WIP

--- src/app/pages/home/user-form/user-form.component.ts
+++ src/app/pages/home/user-form/user-form.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from '../../../services/users.service';
+import { Router } from '@angular/router';
+import { IUser } from '../../../interfaces/iuser.interface';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-user-form',
@@ -9,5 +13,42 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class UserFormComponent {
   @Input() idUser: string = '';
+  user: IUser = {
+    _id: '',
+    id: 0,
+    first_name: '',
+    last_name: '',
+    username: '',
+    email: '',
+    image: '',
+    password: '',
+  };
   usersForm: FormGroup = new FormGroup({}, []);
+  title: string = 'Registrar';
+  userService = inject(UsersService);
+  router = inject(Router);
+
+  async ngOnInit() {
+    if (this.idUser) {
+      // llamamos al servicio y cargamos los datos del empleado
+      try {
+        this.user = await this.userService.getById(this.idUser);
+        this.title = 'Actualizar';
+      } catch (msg: any) {
+        toast.error(msg.error);
+      }
+    }
+
+    this.usersForm = new FormGroup(
+      {
+        nombre: new FormControl(this.user.first_name || '', []),
+        apellidos: new FormControl(this.user.last_name || '', []),
+        email: new FormControl(this.user.email || '', []),
+        imagen: new FormControl(this.user.image || '', []),
+      },
+      []
+    );
+  }
+
+  submit() {}
 }
