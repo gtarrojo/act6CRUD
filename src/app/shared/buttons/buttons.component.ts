38b8c7f9a6901e7eef27c3361847f912b512bf0a
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-03-22 09:12:38 +0100

    add delete button functionality

--- src/app/shared/buttons/buttons.component.ts
+++ src/app/shared/buttons/buttons.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, Input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { IUser } from '../../interfaces/iuser.interface';
+import { UsersService } from '../../services/users.service';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-buttons',
@@ -20,4 +22,22 @@ export class ButtonsComponent {
     password: '',
   };
   @Input() atView: Boolean = false;
+  usersService = inject(UsersService);
+  router = inject(Router);
+
+  deleteUser(id: string): void {
+    toast(
+      `Vas a borrar al usuario ${this.user.first_name} ${this.user.last_name}`,
+      {
+        action: {
+          label: 'Aceptar',
+          onClick: async () => {
+            await this.usersService.deleteById(id);
+
+            this.router.navigate(['/dashboard', 'empleados']);
+          },
+        },
+      }
+    );
+  }
 }
