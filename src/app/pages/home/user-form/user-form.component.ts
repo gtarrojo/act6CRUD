8c55bca598975a3336d445dbf7e8cfca03959496
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-03-22 18:44:15 +0100

    start form component

--- src/app/pages/home/user-form/user-form.component.ts
+++ src/app/pages/home/user-form/user-form.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.css'
+  styleUrl: './user-form.component.css',
 })
 export class UserFormComponent {
-
+  @Input() idUser: string = '';
+  usersForm: FormGroup = new FormGroup({}, []);
 }
